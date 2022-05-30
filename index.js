25966ab0e6a6785ffe61245cabc2d6aa87dfb3a0
Sreenivas <sreenivasadyourworld@gmail.com>
2022-05-30 14:45:24 +0530

    added entertainment category

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 app.use(cors())
 
 const url = 'https://tv9telugu.com/andhra-pradesh/'
+const ent_url= 'https://tv9telugu.com/entertainment'
 
 app.get('/', function (req, res) {
     axios(url)
@@ -52,6 +53,52 @@ app.get('/results/:id', (req, res) => {
         }).catch(err => console.log(err))
 
 })
+
+app.get('/entertainment', function (req, res) {
+    axios(ent_url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('.imgCont', html).each(function () { //<-- cannot be a function expression
+            const title = $(this).find('img').attr('alt')
+            const url = $(this).find('a').attr('href')
+            const img = $(this).find('img').attr('data-src')
+            articles.push({
+                title,
+                url,
+                img
+            })
+        })
+        res.json(articles)
+    }).catch(err => console.log(err))
+
+})
+
+app.get('/entertainment/:id', function (req, res) {
+    let pageNo = req.params.id
+    axios(ent_url+`/${pageNo}`)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('.imgCont', html).each(function () { //<-- cannot be a function expression
+            const title = $(this).find('img').attr('alt')
+            const url = $(this).find('a').attr('href')
+            const img = $(this).find('img').attr('data-src')
+            articles.push({
+                title,
+                url,
+                img
+            })
+        })
+        res.json(articles)
+    }).catch(err => console.log(err))
+
+})
+
 
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
