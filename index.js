249a630703489f0edface30caed5cc76173bd13c
Sreenivas <sreenivasadyourworld@gmail.com>
2022-05-30 14:55:53 +0530

    technology cat added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(cors())
 
 const url = 'https://tv9telugu.com/andhra-pradesh/'
 const ent_url= 'https://tv9telugu.com/entertainment'
+const tech_url = 'https://tv9telugu.com/technology'
 
 app.get('/', function (req, res) {
     axios(url)
@@ -79,6 +80,51 @@ app.get('/entertainment', function (req, res) {
 app.get('/entertainment/:id', function (req, res) {
     let pageNo = req.params.id
     axios(ent_url+`/${pageNo}`)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('.imgCont', html).each(function () { //<-- cannot be a function expression
+            const title = $(this).find('img').attr('alt')
+            const url = $(this).find('a').attr('href')
+            const img = $(this).find('img').attr('data-src')
+            articles.push({
+                title,
+                url,
+                img
+            })
+        })
+        res.json(articles)
+    }).catch(err => console.log(err))
+
+})
+
+app.get('/technology', function (req, res) {
+    axios(tech_url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('.imgCont', html).each(function () { //<-- cannot be a function expression
+            const title = $(this).find('img').attr('alt')
+            const url = $(this).find('a').attr('href')
+            const img = $(this).find('img').attr('data-src')
+            articles.push({
+                title,
+                url,
+                img
+            })
+        })
+        res.json(articles)
+    }).catch(err => console.log(err))
+
+})
+
+app.get('/technology/:id', function (req, res) {
+    let pageNo = req.params.id
+    axios(tech_url+`/${pageNo}`)
     .then(response => {
         const html = response.data
         const $ = cheerio.load(html)
