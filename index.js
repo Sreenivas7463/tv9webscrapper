8bdd6415f51b219851808d57f49e32425aa2a99b
T. Sreenivasulu <59993636+Sreenivas7463@users.noreply.github.com>
2025-06-27 09:56:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -320,7 +320,7 @@ app.get('/entertainment/:id', function (req, res) {
         const $ = cheerio.load(html)
         const articles = []
 
-        $('.imgCont', html).each(function () { //<-- cannot be a function expression
+        $('figure', html).each(function () { //<-- cannot be a function expression
             const title = $(this).find('img').attr('alt')
             const url = $(this).find('a').attr('href')
             const img = $(this).find('img').attr('src')
