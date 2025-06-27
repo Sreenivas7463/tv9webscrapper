0ca7d79ab2d50f71c9e181863f6d22fa2ed2c09e
T. Sreenivasulu <59993636+Sreenivas7463@users.noreply.github.com>
2025-06-27 10:05:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ app.get('/entertainment', function (req, res) {
         $('figure', html).each(function () { //<-- cannot be a function expression
             const title = $(this).find('img').attr('alt')
             const url = $(this).find('a').attr('href')
-            const img = $(this).find('img').attr('data-src')
+            const img = $(this).find('img').attr('src')
             articles.push({
                 title,
                 url,
