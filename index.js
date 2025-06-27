ffc226688375e68e14561735bb18d971374f1ac9
T. Sreenivasulu <59993636+Sreenivas7463@users.noreply.github.com>
2025-06-27 10:11:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -342,7 +342,7 @@ app.get('/technology', function (req, res) {
         const $ = cheerio.load(html)
         const articles = []
 
-        $('.imgCont', html).each(function () { //<-- cannot be a function expression
+        $('figure', html).each(function () { //<-- cannot be a function expression
             const title = $(this).find('img').attr('alt')
             const url = $(this).find('a').attr('href')
             const img = $(this).find('img').attr('src')
